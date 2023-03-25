46024a6f8d9e62a0b6f8f73962545fcd1c157143
nfour <novus.nfour@gmail.com>
2023-03-25 14:52:12 +1000

    Add in a portfolio page

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,8 +1,12 @@
-import { observer } from 'mobx-react-lite'
-import { RootStore } from './state/RootStore'
+import { observer } from 'mobx-react-lite';
+import { PortfolioPage } from './pages/PortfolioPage';
+import { RootStoreProvider } from './state/reactContext';
+import { RootStore } from './state/RootStore';
 
-export const Root = observer<{
-  store: RootStore
-}>(({ store }) => {
-  return <>Root!</>
-})
+export const Root = observer<{ store: RootStore }>(({ store }) => {
+  return (
+    <RootStoreProvider value={store}>
+      <PortfolioPage />
+    </RootStoreProvider>
+  );
+});
