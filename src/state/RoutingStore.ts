4a5e4a1a70ff3888edd7b29bf126f1f03d0d0845
nfour <novus.nfour@gmail.com>
2023-03-30 09:29:46 +1000

    wip

--- src/state/RoutingStore.ts
+++ src/state/RoutingStore.ts
@@ -1,3 +1,4 @@
+import { createBrowserHistory } from 'history';
 import { makeAutoObservable } from 'mobx';
 import { RootStore } from './RootStore';
 
@@ -5,4 +6,27 @@ export class RoutingStore {
   constructor(private root: RootStore) {
     makeAutoObservable(this);
   }
+
+  private history = createBrowserHistory();
+
+  get route(): undefined | Route {
+    const path = this.history.location.pathname;
+
+    if (path === '/')
+      return {
+        path,
+        location: {
+          pathname,
+        },
+      };
+  }
+}
+
+interface Route<PATH = string, PATHNAME = {}, SEARCH = {}, HASH = string> {
+  path: PATH;
+  location: {
+    pathname: PATHNAME;
+    search: SEARCH;
+    hash: HASH;
+  };
 }
